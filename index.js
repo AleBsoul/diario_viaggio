04ff9e3e29b3b3f7582c0b4adbb5f04aa645c6f8
mugnaifederico <mugnaifederico@itis-molinari.eu>
2024-04-29 11:50:10 +0200

    server_func

--- index.js
+++ index.js
@@ -18,3 +18,27 @@ const server = http.createServer(app);
 server.listen(80, () => {
   console.log("- server running");
 });
+
+const mysql = require("mysql2");
+const conf = require("./conf.js")
+const connection = mysql.createConnection(conf);
+
+const executeQuery = (sql) => {
+  return new Promise((resolve, reject) => {
+    connection.query(sql, function (err, result) {
+      if (err) {
+        console.log(err);
+        reject();
+      }
+      resolve(result);
+    });
+  });
+};
+
+executeQuery(`
+CREATE TABLE utenti (
+  id INT PRIMARY KEY AUTO_INCREMENT ,
+  username varchar(255),
+  password varchar(255)
+)`)
+
