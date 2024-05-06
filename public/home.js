c4fcffbd8a265a40f80c181d596267f1c25a275a
mugnaifederico <mugnaifederico@itis-molinari.eu>
2024-05-06 22:03:51 +0200

    nav-collapse

--- public/home.js
+++ public/home.js
@@ -1,12 +1,27 @@
-const template_nav = `
-    <button id="nav-collapsed">
-        <i class="fa fa-bars" aria-hidden="true"></i>
-    </button>
-`
-const right_nav = document.getElementById("right-nav");
-
-window.addEventListener("resize", function(){
-    if(window.innerWidth<800){
-        console.log()
+//navbar
+const rightNavContent = document.getElementById("right-nav-content");
+const bar = document.getElementById("bar");
+const barBtn = document.getElementById("bar-btn");
+const check_size = () =>{
+    if(window.innerWidth<850){
+        bar.classList.remove("invisible");
+        rightNavContent.classList.add("invisible");
+        barBtn.onclick=()=>{
+            if(rightNavContent.classList.contains("invisible")){
+                rightNavContent.classList.remove("invisible");
+                rightNavContent.classList.add("collapsed")
+            }else{
+                rightNavContent.classList.remove("collapsed");
+                rightNavContent.classList.add("invisible");
+            }
+        }
+    }else{
+        bar.classList.add("invisible");
+        rightNavContent.classList.remove("invisible");
+        rightNavContent.classList.remove("collapsed")
     }
+}
+window.addEventListener("resize", function(){
+    check_size();
 })
+check_size();
