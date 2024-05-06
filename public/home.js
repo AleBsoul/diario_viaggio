b8aee0a57e8ebdd56d9ab23570e0737b22cde247
mugnaifederico <mugnaifederico@itis-molinari.eu>
2024-05-06 13:27:38 +0200

    nav

---
+++ public/home.js
@@ -0,0 +1,12 @@
+const template_nav = `
+    <button id="nav-collapsed">
+        <i class="fa fa-bars" aria-hidden="true"></i>
+    </button>
+`
+const right_nav = document.getElementById("right-nav");
+
+window.addEventListener("resize", function(){
+    if(window.innerWidth<800){
+        console.log()
+    }
+})
