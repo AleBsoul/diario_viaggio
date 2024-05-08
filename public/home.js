d6d0cd5790beabccfa815454cf8bbab288a8fea9
mugnaifederico <mugnaifederico@itis-molinari.eu>
2024-05-08 14:28:04 +0200

    newViaggio

--- public/home.js
+++ public/home.js
@@ -51,4 +51,68 @@ loadTravels().then((result)=>{
 })
 
 
+
+//modal new viaggio
+const modal = document.getElementById("modal");
+const closeButton = document.querySelector(".close-button");
+const openModal_btn = document.getElementById("openModal");
+const close_btn = document.getElementById("close_btn");
+
+let isOpened = false;
+
+const openModal = () => {
+  modal.classList.add("is-open");
+};
+
+const closeModal = () => {
+  modal.classList.remove("is-open");
+};
+
+
+
+openModal_btn.onclick=()=>{
+    openModal();
+}
+close_btn.onclick=()=>{
+    closeModal();
+}
+
+
+
+
+//add viaggio
+const newViaggio = document.getElementById("newViaggio_btn");
+const titoloInput = document.getElementById("titolo_viaggio_input");
+const descrInput = document.getElementById("descrizione_viaggio_input");
+const immInput = document.getElementById("immagine_viaggio_input");
+
+const titoloErr = document.getElementById("titolo_error");
+const descrErr = document.getElementById("descrizione_error");
+const immErr = document.getElementById("immagine_error");
+
+newViaggio.onclick=()=>{
+    const titolo = titoloInput.value;
+    if(!titolo){
+        titoloErr.classList.remove("invisible");
+        setTimeout(()=>{
+            titoloErr.classList.add("invisible");
+        },2000)
+    }
+
+    const descrizione = descrInput.value;
+    if(!descrizione){
+        descrErr.classList.remove("invisible");
+        setTimeout(()=>{
+            descrErr.classList.add("invisible");
+        },2000)
+    }
+    const immagine = immInput.value;
+    if(!immagine){
+        immErr.classList.remove("invisible");
+        setTimeout(()=>{
+            immErr.classList.add("invisible");
+        },2000)
+    }
+    console.log(titolo, descrizione, immagine)
+}
   
